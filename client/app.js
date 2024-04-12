cf0af663f1c2d9823bde95623a9573fc305b87b1
Ben <bmshreeve3@gmail.com>
2024-04-12 11:24:38 +0100

    changes

--- client/app.js
+++ client/app.js
@@ -1,17 +1,55 @@
-async function getData() {
-  const response = await fetch("http://localhost:8080/employees");
-  const data = await response.json();
-  console.log(data);
+// async function getData() {
+//   const response = await fetch("http://localhost:8080/employees");
+//   const data = await response.json();
+//   console.log(data);
 
-  for (let i = 0; i < data.length; i++) {
-    const info = data[i];
-    // const p = document.createElement("p");
-    // p.innerHTML =
-    Object.entries(info).forEach(([key, value]) => {
-      console.log(key, value);
-    });
-  }
-}
+//   for (let i = 0; i < data.length; i++) {
+//     const info = data[i];
+//     // const p = document.createElement("p");
+//     // p.innerHTML =
+//     Object.entries(info).forEach(([key, value]) => {
+//       console.log(key, value);
+//     });
+//   }
+// }
 
-//   getMessages();
-getData();
+// //   getMessages();
+// getData();
+
+const form = document.getElementById("submit");
+form.addEventListener("submit", async function (event) {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const formValues = Object.fromEntries(formData);
+
+  const buttonId = document.activeElement.id;
+  const endpoint =
+    buttonId === "signup-btn"
+      ? "http://localhost:8080/sign-up"
+      : "http://localhost:8080/login";
+
+  await fetch(endpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formValues),
+  });
+  form.reset();
+});
+
+// form.addEventListener("submit", async function (event) {
+//     event.preventDefault();
+//     const formData = new FormData(form);
+//     const formValues = Object.fromEntries(formData);
+
+//     await fetch("https://message-board-server.onrender.com/messages", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(formValues),
+//     });
+//     form.reset();
+//     addElements(formValues);
+//   });
--- server/server.js
+++ server/server.js
@@ -16,10 +16,44 @@ app.get("/", function (request, response) {
   response.json("You are looking at my server");
 });
 
-app.get("/employees", async function (request, response) {
-  const result = await db.query(`SELECT * FROM employees`);
+// app.get("/employees", async function (request, response) {
+//   const result = await db.query(`SELECT * FROM employees`);
 
-  response.json(result.rows);
+//   response.json(result.rows);
+// });
+
+app.post("/sign-up", async function (request, response) {
+  const username = request.body.username;
+  const password = request.body.password;
+
+  await db.query(
+    "INSERT INTO username_password (username, password) VALUES ($1, $2)",
+    [username, password]
+  );
+
+  response.json("uploaded");
+});
+
+app.post("/login", async function (request, response) {
+  const username = request.body.username;
+  const password = request.body.password;
+
+  const result = await db.query(
+    "SELECT password FROM username_password WHERE username = $1",
+    [username]
+  );
+
+  if (result.rows.length === 0) {
+    console.log("no user exists");
+  } else {
+    if (password === result.rows[0].password) {
+      console.log("You have logged in");
+    } else {
+      console.log("password incorrect");
+    }
+  }
+
+  response.json("checked");
 });
 
 app.listen(8080, function () {
